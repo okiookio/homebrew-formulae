1cbe401450a9d1e83ddf8608e403d79dc6ecf962
DuongTH.dev <hungduongwp@gmail.com>
2024-05-14 00:21:01 +0700

    Update gyb.rb

--- Formula/gyb.rb
+++ Formula/gyb.rb
@@ -7,13 +7,14 @@ class Gyb < Formula
   homepage "https://nshipster.com/swift-gyb/"
   url "https://raw.githubusercontent.com/apple/swift/main/utils/gyb"
   version "2024-05-14"
-  # sha256 "0a97b3ac257fa29d6240d1878590facd1d80053491cc14601f7e68bb182a29b1"
+  sha256 "5647355be7d91007cc4c849da79bea235a7d7f97871384f0d12f639402d53190"
 
   depends_on xcode: ["15.0", :build]
 
   resource "gyb.py" do
     url "https://raw.githubusercontent.com/apple/swift/main/utils/gyb.py"
     version "2024-05-14"
+    sha256 "dbe94da7e6622b605fea6406aa3ab6e7f64202198f8f351bfe3dab618ae84304"
   end
   depends_on "python@2" if MacOS.version <= :snow_leopard
 
